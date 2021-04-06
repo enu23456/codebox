62895c80f67c87c318aabcea4b5a1462a417d9b2
naga <naga36181@gmail.com>
2021-04-06 17:21:15 +0900

    add file

---
+++ rust/main_bubblesort.rs
@@ -0,0 +1,24 @@
+//! バブルソートを行うプログラム
+//!
+//! 必要な依存関係（Cargo.toml の [dependencies]）
+//! なし
+
+/// 引数で与えられたベクタに対してバブルソートを行う
+///
+fn sort_bubble(array: &mut Vec<i32>) {
+    for i in 0..array.len() {
+        for j in 0..array.len() - i - 1 {
+            if array[j + 1] < array[j] {
+                array.swap(j, j + 1);
+                // println!("{:?}", array);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut array = vec![3, 2, 5, 1, 4];
+    println!("{:?}", array);
+    sort_bubble(&mut array);
+    println!("{:?}", array);
+}
