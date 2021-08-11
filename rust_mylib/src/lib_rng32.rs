434dfe6ba4e794e91919f60f504cdd6f643ec0a9
naga <naga36181@gmail.com>
2021-08-11 16:42:53 +0900

    improve code

--- rust_mylib/src/lib_rng32.rs
+++ rust_mylib/src/lib_rng32.rs
@@ -6,22 +6,19 @@
 //! ## 使用方法
 //! exampleフォルダ内のex_mylib_prng32を確認してください
 //!
-//! ## 所感（やり残しなど含め）
-//! + Rust分かんない
-//! + 全然Rustっぽく書いてない感じが半端ない
-//! + new_with_typeも本当はenumを引数に取りたかったが、そうすると上手くいかない
-//! + 今後もRustを勉強しつつ、本ライブラリをブラッシュアップしていけると良いな
+//! ## 所感
+//! new_with_typeでは、２番目の引数を数値ではなくenumで指定できるようにしたい
 
 pub enum GeneratorType {
-    XORSHIFT128,
     LCG32,
+    XORSHIFT32x4,
+    XOROSHIRO32x4p,
+    XOROSHIRO32x4pp,
+    XOROSHIRO32x4ss,
 }
 
 pub struct RNG32 {
-    alpha: u32,
-    beta: u32,
-    gamma: u32,
-    delta: u32,
+    state: [u32; 4],
     gentype: GeneratorType,
 }
 
@@ -29,35 +26,29 @@ impl RNG32 {
     pub fn new(seed: u32) -> RNG32 {
         let param = generate_param(seed);
         let ret = RNG32 {
-            alpha: param.0,
-            beta: param.1,
-            gamma: param.2,
-            delta: param.3,
-            gentype: GeneratorType::XORSHIFT128,
+            state: param,
+            gentype: GeneratorType::XOROSHIRO32x4ss,
         };
         return ret;
     }
     pub fn new_with_type(seed: u32, type_num: u8) -> RNG32 {
         let param = generate_param(seed);
         let gentype: GeneratorType = match type_num {
-            0 => GeneratorType::XORSHIFT128,
-            _ => GeneratorType::LCG32,
+            0 => GeneratorType::LCG32,
+            1 => GeneratorType::XORSHIFT32x4,
+            2 => GeneratorType::XOROSHIRO32x4p,
+            3 => GeneratorType::XOROSHIRO32x4pp,
+            _ => GeneratorType::XOROSHIRO32x4ss,
         };
         let ret = RNG32 {
-            alpha: param.0,
-            beta: param.1,
-            gamma: param.2,
-            delta: param.3,
+            state: param,
             gentype: gentype,
         };
         return ret;
     }
     pub fn reset(&mut self, seed: u32) {
         let param = generate_param(seed);
-        self.alpha = param.0;
-        self.beta = param.1;
-        self.gamma = param.2;
-        self.delta = param.3;
+        self.state = param;
         return;
     }
     pub fn next(&mut self) -> u32 {
@@ -65,51 +56,92 @@ impl RNG32 {
             GeneratorType::LCG32 => {
                 return self.next_lcg32();
             }
-            GeneratorType::XORSHIFT128 => {
-                return self.next_xorshift128();
+            GeneratorType::XORSHIFT32x4 => {
+                return self.next_xorshift32x4();
+            }
+            GeneratorType::XOROSHIRO32x4p => {
+                return self.next_xoroshiro32x4p();
+            }
+            GeneratorType::XOROSHIRO32x4pp => {
+                return self.next_xoroshiro32x4pp();
+            }
+            _ => {
+                return self.next_xoroshiro32x4ss();
             }
         }
     }
 
     fn next_lcg32(&mut self) -> u32 {
-        self.delta = (((1103515245 * self.delta as u64) + 12345) & 0xFFFF_FFFF) as u32;
-        return self.delta;
+        self.state[3] = (((1103515245 * self.state[3] as u64) + 12345) & 0xFFFF_FFFF) as u32;
+        return self.state[3];
     }
-    fn next_xorshift128(&mut self) -> u32 {
-        let t: u32 = self.alpha ^ (self.alpha << 11);
-        self.alpha = self.beta;
-        self.beta = self.gamma;
-        self.gamma = self.delta;
-        self.delta = (self.delta ^ (self.delta >> 19)) ^ (t ^ (t >> 8));
-        return self.delta;
+    fn next_xorshift32x4(&mut self) -> u32 {
+        let t: u32 = self.state[0] ^ (self.state[0] << 11);
+        self.state[0] = self.state[1];
+        self.state[1] = self.state[2];
+        self.state[2] = self.state[3];
+        self.state[3] = (self.state[3] ^ (self.state[3] >> 19)) ^ (t ^ (t >> 8));
+        return self.state[3];
+    }
+    fn next_xoroshiro32x4p(&mut self) -> u32 {
+        let result: u32 = (self.state[0] as u64 + self.state[3] as u64) as u32;
+        let t: u32 = self.state[1] << 9;
+
+        self.state[2] ^= self.state[0];
+        self.state[3] ^= self.state[1];
+        self.state[1] ^= self.state[2];
+        self.state[0] ^= self.state[3];
+        self.state[2] ^= t;
+        self.state[3] = (self.state[3] << 11) | (self.state[3] >> 21);
+        return result;
+    }
+    fn next_xoroshiro32x4pp(&mut self) -> u32 {
+        let temp: u64 = self.state[0] as u64 + self.state[3] as u64;
+        let result: u32 = (((temp << 7) | (temp >> 25)) * self.state[0] as u64) as u32;
+        let t: u32 = self.state[1] << 9;
+
+        self.state[2] ^= self.state[0];
+        self.state[3] ^= self.state[1];
+        self.state[1] ^= self.state[2];
+        self.state[0] ^= self.state[3];
+        self.state[2] ^= t;
+        self.state[3] = (self.state[3] << 11) | (self.state[3] >> 21);
+        return result;
+    }
+    fn next_xoroshiro32x4ss(&mut self) -> u32 {
+        let temp: u64 = self.state[1] as u64 * 5;
+        let result: u32 = (((temp << 7) | (temp >> 25)) * 9) as u32;
+        let t: u32 = self.state[1] << 9;
+
+        self.state[2] ^= self.state[0];
+        self.state[3] ^= self.state[1];
+        self.state[1] ^= self.state[2];
+        self.state[0] ^= self.state[3];
+        self.state[2] ^= t;
+        self.state[3] = (self.state[3] << 11) | (self.state[3] >> 21);
+        return result;
     }
 }
 
-fn generate_param(seed: u32) -> (u32, u32, u32, u32) {
-    let mut ret = (0u32, 0u32, 0u32, 0u32);
+fn generate_param(seed: u32) -> [u32; 4] {
+    let mut ret = [0; 4];
 
     let mut k = seed;
     let mut h = 2166136261;
-    k = (k as u64 * 0xcc9e2d51) as u32;
+    k = (k as u64 * 3432918353) as u32;
     k = (k << 15) | (k >> 17);
-    k = (k as u64 * 0x1b873593) as u32;
+    k = (k as u64 * 461845907) as u32;
     h ^= k;
     h = (h << 13) | (h >> 19);
-    h = ((h as u64) * 5 + 0xe6546b64) as u32;
+    h = ((h as u64) * 5 + 3864292196) as u32;
 
     for i in 0..4 {
         h ^= h >> 16;
-        h = (h as u64 * 0x85ebca6b) as u32;
+        h = (h as u64 * 2246822507) as u32;
         h ^= h >> 13;
-        h = (h as u64 * 0xc2b2ae35) as u32;
+        h = (h as u64 * 3266489909) as u32;
         h ^= h >> 16;
-        // Rustのタプルはret.iのようにはできないための苦肉の策
-        match i {
-            0 => ret.0 = h,
-            1 => ret.1 = h,
-            2 => ret.2 = h,
-            _ => ret.3 = h,
-        }
+        ret[i] = h;
     }
     return ret;
 }
@@ -118,9 +150,14 @@ mod tests {
     use super::*;
     #[test]
     fn test_lib_rng32() {
-        let mut rnd = RNG32::new(0);
-        assert_eq!(3598290415, rnd.next());
-        assert_eq!(1646212294, rnd.next());
-        assert_eq!(1022361848, rnd.next());
+        let mut rng_lcg32 = RNG32::new_with_type(0, 0);
+        assert_eq!(2958721311, rng_lcg32.next());
+        assert_eq!(1725602924, rng_lcg32.next());
+        assert_eq!(3460674101, rng_lcg32.next());
+
+        let mut rng_xoroshiro32x4ss = RNG32::new_with_type(0, 255);
+        assert_eq!(4105684694, rng_xoroshiro32x4ss.next());
+        assert_eq!(3685076946, rng_xoroshiro32x4ss.next());
+        assert_eq!(3332898383, rng_xoroshiro32x4ss.next());
     }
 }
